12452a67feecd26a9f5ca5ccac68c4c7c5c6e60d
Andrei Bocan <zmaxor@gmail.com>
2010-09-11 01:18:22 +0300

    Animate!

--- js/zirkel.js
+++ js/zirkel.js
@@ -4,7 +4,7 @@
   var circle = Math.PI * 2;
   var buffer = document.createElement('div');
   var base_colors = ['#F00', '#0F0', '#00F'];
-  var last_value = null;
+  var last_color = null;
 
   function drawCircle(x, y, color, start, length, total) {
     start = start || 0;
@@ -33,14 +33,41 @@
     return colors.split(', ').map(function(e) { return parseInt(e, 10) });
   }
 
-  function drawColorBreakdown(color) {
+  function animateBreakDown(last_color, this_color) {
+    var current_frame = 0;
+    var total_frames = 100;
+    var frame_colors = [0];
+    var r_step = (last_color[0] - this_color[0]) / total_frames;
+    var g_step = (last_color[1] - this_color[1]) / total_frames;
+    var b_step = (last_color[2] - this_color[2]) / total_frames;
+
+    var timer = setInterval(function() {
+      current_frame += 1;
+      frame_colors[1] = last_color[0] - ~~(r_step * current_frame);
+      frame_colors[2] = last_color[1] - ~~(g_step * current_frame);
+      frame_colors[3] = last_color[2] - ~~(b_step * current_frame);
+      fillCircle(100, 100, 'rgb( '+frame_colors[1] + ',' + frame_colors[2] + ',' + frame_colors[3] + ')');
+      drawColorBreakdown(frame_colors)
+      console.log(frame_colors);
+      if ( current_frame == 100 ) {
+        clearInterval(timer);
+      }
+    }, 30);
+
+  }
+
+  function drawColor(color) {
     var colors = breakColor(color);
+    colors.unshift(0);
+    drawColorBreakdown(colors);
+  }
+
+  function drawColorBreakdown(colors) {
     var total = colors.reduce(function(before, now) { return before + now });
 
     context.clearRect(0, 0, canvas.width, canvas.height);
     fillCircle(100, 100, color);
 
-    colors.unshift(0);
     colors.reduce( function(last, current, index) {
       if ( current != 0 ) {
         drawCircle(100, 100, base_colors[index-1], last, current, total);
@@ -55,7 +82,12 @@
 
   form.addEventListener('submit', function(e) {
     e.preventDefault();
-    drawColorBreakdown(color_element.value);
+    var this_color = breakColor(color_element.value);
+    if ( last_color != null ) {
+      animateBreakDown(last_color, this_color);
+    }
+
+    //drawColorBreakdown(color_element.value);
 
     last_color = breakColor(color_element.value);
   }, false);
